8c783ef8c2d412349544ce1f7210113f46963a5b
Rosti1126 <Rosti1126@gmail.com>
2021-06-02 22:13:35 -0400

    Quotes Added

--- js/script.js
+++ js/script.js
@@ -11,6 +11,40 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
+const quotes = [
+    {
+       quote: 'Be the change that you wish to see in the world.',
+       source: 'Mahatma Gandhi',
+       citation:'',
+       year:1908
+    },
+    {
+       quote: 'If you tell the truth, you do not have to remember anything.',
+       source: 'Mark Twain',
+       citation:'The Adventures of Tom Sawyer',
+       year:1876
+    },
+    {
+       quote: 'Always forgive your enemies; nothing annoys them so much.',
+       source: 'Oscar Wilde',
+       citation:'',
+       year:1876
+    },
+    {
+       quote: 'You miss 100 percent of the shots you never take',
+       source: 'Wayne Gretzky',
+       citation:'',
+       year:1997
+    },
+    {
+       quote: 'Life is what happens when you’re busy making other plans.',
+       source: 'John Lennon',
+       citation:'Beautiful boy',
+       year:1980
+    }
+    
+];
+
 
 
 /***
